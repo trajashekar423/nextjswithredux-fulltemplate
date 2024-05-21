710a755b7841c723b5fe2cc17cfdd8bf40c0e0ac
trajashekar423 <trajashekar423@gmail.com>
2024-05-21 23:49:17 +0530

    iamge

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useSelector } from "react-redux";
 
-
+import { Image } from "next/image";
 export default function Home() {
   const content = useSelector((state) => state.content.homepageContent);
   const { welcomeMessage, Features,herocontent,button1,button2 } = content;
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="row">
     <div className="col">
     <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+    <Image class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
     <h1 class="display-5 fw-bold text-body-emphasis">{welcomeMessage}</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">{herocontent}</p>
