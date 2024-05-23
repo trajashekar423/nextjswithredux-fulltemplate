ac1a12c2c6c072e822b4f55135dd9bd26ab57b96
trajashekar423 <trajashekar423@gmail.com>
2024-05-23 17:13:25 +0530

    f

--- src/app/maincomponents/footer.js
+++ src/app/maincomponents/footer.js
@@ -12,7 +12,6 @@ function Footer() {
     <div>
       <div className="container">
   <footer className="py-3 my-4">   
- 
            
               <ul className='nav justify-content-center border-bottom pb-3 mb-3'>       
               {footerlinks.map(myfooter => (
