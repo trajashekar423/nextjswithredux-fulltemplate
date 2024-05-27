35802fa28dc02ddb406bdccaf4d07bd5b1561903
trajashekar423 <trajashekar423@gmail.com>
2024-05-27 22:51:55 +0530

    G

--- src/app/newsapi/page.js
+++ src/app/newsapi/page.js
@@ -8,7 +8,7 @@ const Newsitems = ({title,description}) => {
   return (
     <div>
       <div className="card">
-  <img src="..." className="card-img-top" alt="..."/>
+
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
